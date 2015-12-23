70c76c85219f9bd53371ddb164722733a080949b
Michael Truog <mjtruog@gmail.com>
2015-12-23 13:05:27 -0800

    Version 1.5.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CPG.Mixfile do
 
   def project do
     [app: :cpg,
-     version: "1.5.0",
+     version: "1.5.1",
      language: :erlang,
      description: description,
      package: package,
@@ -11,9 +11,9 @@ defmodule CPG.Mixfile do
   end
 
   defp deps do
-    [{:trie, "~> 1.5.0"},
-     {:reltool_util, "~> 1.5.0"},
-     {:quickrand, "~> 1.5.0"}]
+    [{:trie, "~> 1.5.1"},
+     {:reltool_util, "~> 1.5.1"},
+     {:quickrand, "~> 1.5.1"}]
   end
 
   defp description do
@@ -22,7 +22,7 @@ defmodule CPG.Mixfile do
 
   defp package do
     [files: ~w(src include doc rebar.config README.md LICENSE NOTICE),
-     contributors: ["Michael Truog"],
+     maintainers: ["Michael Truog"],
      licenses: ["BSD"],
      links: %{"GitHub" => "https://github.com/okeuday/cpg"}]
    end
