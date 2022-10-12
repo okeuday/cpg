e527187bd620df8664dd478bf781570249cc4a17
Michael Truog <mjtruog@protonmail.com>
2022-10-11 19:50:19 -0700

    Version 2.0.5.

--- mix.exs
+++ mix.exs
@@ -6,10 +6,11 @@ defmodule CPG.Mixfile do
 
   def project do
     [app: :cpg,
-     version: "2.0.4",
+     version: "2.0.5",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
+       :deterministic,
        :debug_info,
        :warn_export_vars,
        :warn_unused_import,
@@ -39,9 +40,9 @@ defmodule CPG.Mixfile do
   end
 
   defp deps do
-    [{:trie, ">= 2.0.4"},
-     {:reltool_util, ">= 2.0.4"},
-     {:quickrand, ">= 2.0.4"}]
+    [{:trie, ">= 2.0.5"},
+     {:reltool_util, ">= 2.0.5"},
+     {:quickrand, ">= 2.0.5"}]
   end
 
   defp description do
